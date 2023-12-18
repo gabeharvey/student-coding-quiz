be96371105a42c0c191a24d448dc81dfad2f5661
Gabe Harvey <harvey7@gmail.com>
2023-12-18 01:01:56 -0600

    local storage added to final.js

--- assets/js/final.js
+++ assets/js/final.js
@@ -2,23 +2,24 @@ const username = document.getElementById("username");
 const saveFinalPoints = document.getElementById("savePointsBtn");
 const finalPoints = document.getElementById("finalPoints");
 const mostRecentPoints = localStorage.getItem("mostRecentPoints");
-
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
+const maximumHighScores = 3;
 
 finalPoints.innerText = mostRecentPoints;
 
 username.addEventListener("keyup", () => {
     savePointsBtn.disabled = !username.value;
-})
+});
 
 saveHighScore = e => {
-    console.log("clicked save");
     e.preventDefault();
     const points = {
         points: mostRecentPoints,
         name: username.value
     };
     highScores.push(points);
-    console.log(highScores);
-}
+    highScores.sort((a,b) => b.score - a.score);
+    highScores.splice(3);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("./index.html");
+};
